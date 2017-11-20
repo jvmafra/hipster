651b69c35532c53dabdd0c04a2ff540474a40a2a
unknown <rafael.sousa@ccc.ufcg.edu.br>
2017-11-20 19:37:59 -0200

    Init handling error with semantic

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -2,7 +2,8 @@ import { Component, ElementRef, OnInit, ViewChild, ViewEncapsulation } from '@an
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { TranslateService } from '@ngx-translate/core';
 import { UserService } from '../services/user.service';
-
+declare var jquery:any;
+declare var $ :any;
 
 @Component({
   selector: 'app-register',
@@ -11,6 +12,7 @@ import { UserService } from '../services/user.service';
   encapsulation: ViewEncapsulation.None
 })
 export class RegisterComponent implements OnInit {
+  private $ : any;
   private user : Object;
   private day : String;
   private month : String;
@@ -28,23 +30,71 @@ export class RegisterComponent implements OnInit {
     this.days = Array.from(Array(31).keys());
     this.months = Array.from(Array(12).keys());
     this.years = this.userService.getBirthdayYearsArray('1905');
+
   }
 
   ngOnInit() {
+    this.initSemanticValidationForm();
   }
 
   private registerUser(user) {
     user.birthDate = this.userService.getBirthDate(this.day, this.month, this.year);
-    console.log(user.birthDate)
-    this.userService.registerUser(user).subscribe(
-      data => {
-        window.location.href = "/user/" + user.username;
-      }, err => {
-        //handle error
-        //@TODO: Need to do this part
-        console.log(err);
+
+    if ($('form').form('is valid')) {
+      this.userService.registerUser(user).subscribe(
+        data => {
+          window.location.href = "/user/" + user.username;
+        }, err => {
+          //handle error
+          //@TODO: Need to do this part
+          console.log(err);
+        }
+      );
+    } else {
+    }
+  }
+
+  private initSemanticValidationForm() {
+    $('.ui.form')
+    .form({
+      inline : true,
+      fields: {
+        name: {
+          rules: [
+          {
+            type   : 'empty',
+            prompt : "fff"
+          }
+          ]
+        },
+        username: {
+          rules: [
+          {
+            type   : 'empty',
+            prompty: 'message'
+          }
+          ]
+        },
+        email: {
+          rules: [
+          {
+            type   : 'empty',
+            prompty: 'message'
+          }
+          ]
+        },
+        password: {
+          rules: [
+          {
+            type   : 'empty',
+            prompty: 'message'
+          }
+          ]
+        },
       }
-    );
+    });
+
+
   }
 
 }
