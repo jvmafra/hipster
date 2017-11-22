44aa5d5d55c72caf29534dde1fbb7548c740d4d2
gileadekelvin <gileadekelvin@gmail.com>
2017-11-22 00:51:12 -0300

    Removendo vestígios da implementação do auth0

--- server/config.js
+++
@@ -1,6 +0,0 @@
-// server/config.js
-module.exports = {
-    AUTH0_DOMAIN: 'hispter.auth0.com', // e.g., kmaida.auth0.com
-    AUTH0_API_AUDIENCE: 'http://localhost:3000/api/', // e.g., 'http://localhost:8083/api/'
-    NAMESPACE: 'http://hipster.com/roles'    
-  };
