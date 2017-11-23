bcbaf0357a59fc51487aee1cd08af55fc75bc7bb
gileadekelvin <gileadekelvin@gmail.com>
2017-11-23 01:22:05 -0300

    Deploy do hipster

--- src/app/services/global.service.ts
+++ src/app/services/global.service.ts
@@ -5,7 +5,7 @@ export class GlobalService {
   private serverHost: String;
 
   constructor() {
-    this.serverHost = 'http://127.0.0.1:3000/api/';
+    this.serverHost = 'https://mysterious-waters-22741.herokuapp.com/';
   }
 
   public getServerHost() {
