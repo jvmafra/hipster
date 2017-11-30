2969fe37866c715be9daacc263e2554247af2fd5
Gabriel Guimarães <gabrielguimoliver@gmail.com>
2017-11-30 01:26:16 -0300

    pagina do post individual

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -6,11 +6,13 @@ import { ProfilePageComponent } from './profile-page/profile-page.component';
 import { RegisterComponent } from './register/register.component';
 import { AuthService } from './services/auth.service';
 import { TimelineComponent } from './timeline/timeline.component';
+import { PostPageComponent } from './post-page/post-page.component';
 
 export const router: Routes = [
   { path: '', component: TimelineComponent, canActivate: [AuthService] },
   { path: 'register', component: RegisterComponent, canActivate: [AuthService] },
   { path: 'user/:username', component: ProfilePageComponent, canActivate: [AuthService] },
+  { path: 'post/:id', component: PostPageComponent, canActivate: [AuthService] },
   { path: '**', redirectTo: '' }
 ];
 
--- src/app/post-page/post-page.component.ts
+++ src/app/post-page/post-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../services/user.service';
+
+declare var jquery:any;
+declare var $ :any;
 
 @Component({
   selector: 'app-post-page',
@@ -8,9 +13,39 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class PostPageComponent implements OnInit {
 
-  constructor() { }
+  private post: any;
+
+  constructor(private route: ActivatedRoute,
+              private userService: UserService) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => {
+        let postId = params['id'];
+
+        this.post = {
+          id: 0,
+          owner_username: "gab",
+          url: "https://www.youtube.com/watch?v=mN1hJXzJyNU",
+          title: "HONNE - SOMEONE THAT LOVES YOU | A COLORS SHOW",
+          description: "We had the great pleasure to welcome HONNE in our studio! The UK electro-soul duo brought the wonderful Naomi along to perform their new single 'Someone That Loves You' (release date: 22.7.)!",
+          creationDate: "28/11/2017",
+          genres: ["Pop"],
+          aproves: ["gab", "rafa", "gila", "mafra", "job", "guga"]
+        };
+
+        var url = this.post.url;
+        var videoid = url.match(/(?:https?:\/{2})?(?:w{3}\.)?youtu(?:be)?\.(?:com|be)(?:\/watch\?v=|\/)([^\s&]+)/);
+        this.post.videoId = videoid[1]
+        $('.ui.embed').embed({
+          source      : 'youtube',
+          id          : this.post.videoId,
+          icon        : 'video'
+        });
+    });
+  }
+
+  getClass(genre) {
+    return this.userService.getColor(genre);
   }
 
 }
