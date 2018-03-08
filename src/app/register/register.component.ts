b93f479ef628ddefe84ce8805a5e903d099a81c1
Gabriel Guimarães <gabrielguimoliver@gmail.com>
2018-03-08 17:38:47 -0300

    login com api do google e facebook

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -8,6 +8,7 @@ import { AlertService } from '../services/alert.service';
 
 declare var jquery:any;
 declare var $ :any;
+declare var firebase: any;
 
 @Component({
   selector: 'app-register',
@@ -25,6 +26,8 @@ export class RegisterComponent implements OnInit {
   public months : Array<number>;
   public years : Array<number>;
   private errorInfo: Array<Object>;
+  private provider : any;
+  private self: any;
 
   constructor(private http: HttpClient,
               private userService: UserService,
@@ -42,6 +45,95 @@ export class RegisterComponent implements OnInit {
   ngOnInit() {
     this.initDateDropdown();
     this.initSemanticValidationFormInfo();
+  }
+
+  private loginWithGoogle(callback: (email, birthday, name)=>void) {
+    this.provider = new firebase.auth.GoogleAuthProvider();
+
+    this.provider.addScope('profile');
+    this.provider.addScope('email');
+    this.provider.addScope('https://www.googleapis.com/auth/user.birthday.read');
+
+    this.provider.setCustomParameters({
+      'display': 'popup'
+    });
+
+    firebase.auth().signInWithPopup(this.provider).then(function(result) {
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var profile = result.additionalUserInfo.profile;
+      // ...
+      var email = profile.email;
+      var birthday = profile.birthday;
+      var name = profile.name;
+
+      callback(email, birthday, name);
+
+    }).catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+
+  }
+
+  private loginWithFacebook(callback: (email, birthday, name)=>void) {
+    this.provider = new firebase.auth.FacebookAuthProvider();
+
+    this.provider.addScope('user_birthday');
+    this.provider.addScope('email');
+    this.provider.addScope('public_profile');
+
+    this.provider.setCustomParameters({
+      'display': 'popup'
+    });
+
+    firebase.auth().signInWithPopup(this.provider).then(function(result) {
+      // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+      var token = result.credential.accessToken;
+      // The signed-in user info.
+      var profile = result.additionalUserInfo.profile;
+      // ...
+      var email = profile.email;
+      var birthday = profile.birthday;
+      var name = profile.name;
+
+      callback(email, birthday, name);
+
+    }).catch(function(error) {
+      console.log(error);
+    });
+  }
+
+  loginGoogle() {
+    this.loginWithGoogle(this.setUpProfile.bind(this));
+  }
+
+  loginFB() {
+    this.loginWithFacebook(this.setUpProfile.bind(this));
+  }
+
+  public setUpProfile(email, birthday, name) {
+
+    this.user.name = name;
+    this.user.email = email;
+
+    if (birthday) {
+      this.day = "" + parseInt(birthday.split("/")[1]);
+      this.month = "" + parseInt(birthday.split("/")[0]);
+      this.year = birthday.split("/")[2];
+      $("#day_r").dropdown("set selected", this.day);
+      $("#month_r").dropdown("set selected", this.month);
+      $("#year_r").dropdown("set selected", this.year);
+    }
+
+    $("#username_r").focus().select().click();
   }
 
   public registerUser(user, event) {
@@ -74,6 +166,9 @@ export class RegisterComponent implements OnInit {
 
   private initDateDropdown() {
     $('.ui.dropdown').dropdown();
+    $('#day_r').dropdown();
+    $('#month_r').dropdown();
+    $('#year_r').dropdown();
   }
 
   private initSemanticValidationFormInfo() {
